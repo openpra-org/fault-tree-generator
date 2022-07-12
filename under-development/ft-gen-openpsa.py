2db1854167e87fb73a233f39231f8cc08675720f
egemen <emaras@ncsu.edu>
2022-07-12 12:40:37 -0400

    The logic is modified and while loop is added to the generators

--- under-development/ft-gen-openpsa.py
+++ under-development/ft-gen-openpsa.py
@@ -2,49 +2,96 @@
 <Egemen M. Aras/>
 This script creates fault-trees in the format of open-psa mef.
 """
-#importing The ElemtTree XML API
+
+import os
 import xml.etree.ElementTree as ET
+import re
 import random
-random.seed(10)
+random.seed(123)
 
-gateList = ['or']
+# pathXFTAModels ="C:/Users/emaras/Desktop/Repo/Gitlab/openpra/ft_solver/models/xfta"
+# pathSCRAMModels = "C:/Users/emaras/Desktop/Repo/Gitlab/openpra/ft_solver/models/scram"
+# pathSAPHSOLVEModels = "C:/Users/emaras/Desktop/Repo/Gitlab/openpra/ft_solver/models/saphsolve"
+#
+# pathXFTAResults ="C:/Users/emaras/Desktop/Repo/Gitlab/openpra/ft_solver/results/xfta"
+# pathSCRAMResults = "C:/Users/emaras/Desktop/Repo/Gitlab/openpra/ft_solver/results/scram"
+# pathSAPHSOLVEResults = "C:/Users/emaras/Desktop/Repo/Gitlab/openpra/ft_solver/results/saphsolve"
 
-def generateXML(fileName):
+# XFTAExecDir = "C:/Users/emaras/Desktop/Repo/xfta/xfta-1-3-1-win32/xftar"
+# def runXFTA(XFTAConfigName):
+#     commandToRunXFTA = XFTAExecDir +" "+ XFTAConfigName
+#     print(commandToRunXFTA)
+#     os.system(commandToRunXFTA)
+#
+# def runSCRAM(inputFile):
+#     commandToRunSCRAM = "docker run -it --rm -v " + os.getcwd() + "/models/scram:/scram supra scram /scram/" + inputFile + "--mocus --probability --importance > models/scram/" + inputFile + ".txt"
+#     print(commandToRunSCRAM)
+#     os.system(commandToRunSCRAM)
 
-    root = ET.Element('opsa-mef')
-    faultTreeName = ET.SubElement(root,'define-fault-tree',{'name':'FT'})
-    modelData = ET.SubElement(root, 'model-data')
+gateList = ['or','and']
+gateListOR = ['or']
+gateListAND = ['and']
+n = 1000
+while (n<=50000):
 
-    n = int(input('Please enter the number of gates :\t'))
-    if n == 0:
-        print("Please a number between 1 and 495!")
-    for gateNumber in range(1,n+1):
-        if gateNumber ==(n):
-            defineGate = ET.SubElement(faultTreeName, 'define-gate', {'name': 'g' + str(gateNumber)})
-            gateType = ET.SubElement(defineGate, random.choice(gateList))
-            basicEvent1 = ET.SubElement(gateType, 'basic-event', {'name': 'be' + str(gateNumber)})
-            basicEvent2 = ET.SubElement(gateType, 'basic-event', {'name': 'bee' + str(gateNumber)})
-            #gate = ET.SubElement(gateType, 'gate', {'name': 'g' + str(gateNumber + 1)})
+    def configFileGenXFTA(name):
 
-            basicEventValue1 = ET.SubElement(modelData, 'define-basic-event', {'name': 'be' + str(gateNumber)})
-            basicEventValu1Assign = ET.SubElement(basicEventValue1, 'float', {'value': str(0.01)})
-            basicEventValue2 = ET.SubElement(modelData, 'define-basic-event', {'name': 'bee' + str(gateNumber)})
-            basicEventValu2Assign = ET.SubElement(basicEventValue2, 'float', {'value': str(0.01)})
-        else:
-            defineGate = ET.SubElement(faultTreeName, 'define-gate', {'name': 'g'+str(gateNumber)})
-            gateType = ET.SubElement(defineGate,random.choice(gateList))
-            basicEvent1 =ET.SubElement(gateType,'basic-event',{'name':'be'+str(gateNumber)})
-            basicEvent2 = ET.SubElement(gateType, 'basic-event', {'name': 'bee' + str(gateNumber)})
-            gate = ET.SubElement(gateType,'gate',{'name':'g'+str(gateNumber+1)})
+        with open('xfta.template', 'r') as file:
+            filedata = file.read()
+        filedata = re.sub("NAME", name, filedata)
+        with open("xfta-" + name, 'w') as file:
+            file.write(filedata)
 
-            basicEventValue1 = ET.SubElement(modelData, 'define-basic-event', {'name': 'be' + str(gateNumber)})
-            basicEventValu1Assign = ET.SubElement(basicEventValue1,'float', {'value': str(0.01)})
-            basicEventValue2 = ET.SubElement(modelData, 'define-basic-event', {'name': 'bee' + str(gateNumber)})
-            basicEventValu2Assign = ET.SubElement(basicEventValue2,'float', {'value': str(0.01)})
+    def generateXML(fileName):
+
+        root = ET.Element('opsa-mef')
+        faultTreeName = ET.SubElement(root,'define-fault-tree',{'name':'FT'})
+        modelData = ET.SubElement(root, 'model-data')
 
 
-    tree = ET.ElementTree(root)
-    tree.write(fileName, xml_declaration=True, encoding='utf-8')
+        for gateNumber in range(1,n+1):
+            if gateNumber ==(n):
+                defineGate = ET.SubElement(faultTreeName, 'define-gate', {'name': 'g' + str(gateNumber)})
+                gateType = ET.SubElement(defineGate, random.choice(gateListAND))
+                basicEvent1 = ET.SubElement(gateType, 'basic-event', {'name': 'be' + str(gateNumber)})
+                basicEvent2 = ET.SubElement(gateType, 'basic-event', {'name': 'bee' + str(gateNumber)})
+                #gate = ET.SubElement(gateType, 'gate', {'name': 'g' + str(gateNumber + 1)})
 
-if __name__=='__main__':
-    generateXML('base-fault-tree.xml')
+                basicEventValue1 = ET.SubElement(modelData, 'define-basic-event', {'name': 'be' + str(gateNumber)})
+                basicEventValu1Assign = ET.SubElement(basicEventValue1, 'float', {'value': str(0.05)})
+                basicEventValue2 = ET.SubElement(modelData, 'define-basic-event', {'name': 'bee' + str(gateNumber)})
+                basicEventValu2Assign = ET.SubElement(basicEventValue2, 'float', {'value': str(0.05)})
+            elif gateNumber%2 ==0:
+                defineGate = ET.SubElement(faultTreeName, 'define-gate', {'name': 'g' + str(gateNumber)})
+                gateType = ET.SubElement(defineGate, random.choice(gateListAND))
+                basicEvent1 = ET.SubElement(gateType, 'basic-event', {'name': 'be' + str(gateNumber)})
+                basicEvent2 = ET.SubElement(gateType, 'basic-event', {'name': 'bee' + str(gateNumber)})
+                gate = ET.SubElement(gateType, 'gate', {'name': 'g' + str(gateNumber + 1)})
+
+                basicEventValue1 = ET.SubElement(modelData, 'define-basic-event', {'name': 'be' + str(gateNumber)})
+                basicEventValu1Assign = ET.SubElement(basicEventValue1, 'float', {'value': str(0.05)})
+                basicEventValue2 = ET.SubElement(modelData, 'define-basic-event', {'name': 'bee' + str(gateNumber)})
+                basicEventValu2Assign = ET.SubElement(basicEventValue2, 'float', {'value': str(0.05)})
+            else:
+                defineGate = ET.SubElement(faultTreeName, 'define-gate', {'name': 'g' + str(gateNumber)})
+                gateType = ET.SubElement(defineGate, random.choice(gateListOR))
+                basicEvent1 = ET.SubElement(gateType, 'basic-event', {'name': 'be' + str(gateNumber)})
+                basicEvent2 = ET.SubElement(gateType, 'basic-event', {'name': 'bee' + str(gateNumber)})
+                gate = ET.SubElement(gateType, 'gate', {'name': 'g' + str(gateNumber + 1)})
+
+                basicEventValue1 = ET.SubElement(modelData, 'define-basic-event', {'name': 'be' + str(gateNumber)})
+                basicEventValu1Assign = ET.SubElement(basicEventValue1, 'float', {'value': str(0.05)})
+                basicEventValue2 = ET.SubElement(modelData, 'define-basic-event', {'name': 'bee' + str(gateNumber)})
+                basicEventValu2Assign = ET.SubElement(basicEventValue2, 'float', {'value': str(0.05)})
+
+
+
+        tree = ET.ElementTree(root)
+        tree.write(fileName, xml_declaration=True, encoding='utf-8')
+
+    if __name__=='__main__':
+        generateXML('ft-openpsa-'+str(n)+'Gates.xml')
+        configFileGenXFTA('ft-openpsa-'+str(n)+'Gates.xml')
+        # runXFTA(pathXFTAModels + " " + "xfta-ft-openpsa-"+str(n)+"Gates.xml")
+        # runSCRAM('ft-openpsa-'+str(n)+'Gates.xml')
+    n+=1000
