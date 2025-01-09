415cccf464d21afa896b707b9cef44401b715f6f
Egemen Aras <emaras@ncsu.edu>
2025-01-09 12:26:01 -0500

    - added `event-tree-generator` argument - directly creating fault-tree logic and data as strings and capturing it by updating `get_printer` function

--- src/fault_tree_generator.py
+++ src/fault_tree_generator.py
@@ -1235,6 +1235,9 @@ def manage_cmd_args(argv=None):
     parser.add_argument("--nest",
                         action="store_true",
                         help="nest NOT connectives in Boolean formulae")
+    parser.add_argument("--event_tree_generator",
+                        action="store_true",
+                        help="generate event tree model")
     args = parser.parse_args(argv)
     return args
 
@@ -1281,7 +1284,10 @@ def fault_tree_generator(argv=None):
     factors = setup_factors(args)
     fault_tree = generate_fault_tree(args.ft_name, args.root, factors)
     printer, captured_object = get_printer(args.out)
-    if args.aralia:
+    if args.event_tree_generator:
+        fault_tree.to_fault_tree_logic(printer, args.nest)
+        return captured_object.getvalue()
+    elif args.aralia:
         fault_tree.to_aralia(printer)
     elif args.SAPHIRE_json_object:
          write_info_SAPHSOLVE_JSON_object(fault_tree, printer, args.seed)
@@ -1294,12 +1300,9 @@ def fault_tree_generator(argv=None):
         write_info_JSON_printer(fault_tree, printer, args.seed)
         fault_tree.to_SAPHIRE_json_printer(printer, args.nest)
     else:
-        # write_info(fault_tree, printer, args.seed)
-        # write_summary(fault_tree, printer)
-        fault_tree.to_xml(printer, args.nest)
-    # print(captured_object.getvalue())
-    return captured_object.getvalue()
-
+        write_info(fault_tree, printer, args.seed)
+        write_summary(fault_tree, printer)
+        fault_tree.to_fault_tree_logic(printer, args.nest)
 
 # def get_printer(file_path=None):
 #     """Returns printer to stream output."""
